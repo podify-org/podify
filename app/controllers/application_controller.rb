d43e63740af138a65aa9d8020dac54c9653dcdb3
Max Hollmann <maxhollmann@gmail.com>
2020-04-13 14:36:52 +0200

    Move stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   before_action do
     Dry::View.reset_cache! # TODO: only in development
   end
--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,4 +2,9 @@ class SourcesController < ApplicationController
   def index
     render html: Views::Sources::Index.new.call.to_s.html_safe
   end
+
+  def create
+    attrs = resolve('source_contract').call(params[:source])
+    pp attrs
+  end
 end
