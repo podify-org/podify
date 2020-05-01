161afb66c20514202303661db83d5e1939774ef2
Max Hollmann <maxhollmann@gmail.com>
2020-05-01 20:27:20 +0200

    Show feed url in index

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'feeds/:collection', to: 'feeds#show', constraints: { format: :rss }
+  get 'feeds/:collection', to: 'feeds#show', as: :feed, constraints: { format: :rss }
 
   mount Sidekiq::Web => '/sidekiq'
 end
