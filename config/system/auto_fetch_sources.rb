838d76346a8518478ab30790f22159c431118ace
Max Hollmann <maxhollmann@gmail.com>
2020-05-02 18:37:48 +0200

    Queue FetchSourceJob directly from Sources::Create

--- config/system/auto_fetch_sources.rb
+++
@@ -1,9 +0,0 @@
-Dry::Rails.container do
-  boot(:auto_fetch_sources) do
-    start do
-      Podify::Container['events'].subscribe 'sources.created' do |event|
-        Podify::Container['downloader.fetch_source_job'].perform_async(event[:source].id)
-      end
-    end
-  end
-end
--- lib/sources/create.rb
+++ lib/sources/create.rb
@@ -2,12 +2,17 @@ module Sources
   class Create
     include Dry::Monads[:result, :do]
 
-    include Podify::Import['events', 'sources.contract']
+    include Podify::Import[
+      'events',
+      'sources.contract',
+      'downloader.fetch_source_job',
+    ]
 
     def call(attrs)
       attrs = yield(contract.call(attrs).to_monad).to_h
       source = yield create_source(attrs)
       events.publish('sources.created', source: source)
+      fetch_source_job.perform_async(source.id)
       Success(source)
     end
 
