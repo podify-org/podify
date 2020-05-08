fdd5a5660515efedb3cef87f4d760501c8ded12e
Max Hollmann <maxhollmann@gmail.com>
2020-05-08 22:20:53 +0200

    Toggeable sidebar

--- app/javascript/components/navbar.vue
+++ app/javascript/components/navbar.vue
@@ -1,24 +1,44 @@
 <template>
 
-  <div class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">
-        <img class="logo" src="http://podify.localhost:3000/packs/media/logo/navbar-9431a977.png">
-      </a>
+<div>
+  <div class="navbar navbar-dark fixed-top bg-dark">
+    <button class="navbar-toggler"
+            type="button"
+            aria-controls="navbar-menu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            @click="toggleSidebar()"
+            >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" rel="nofollow" data-method="delete" href="/user/sign_out">
-            Sign out
-          </a>
-        </li>
-      </ul>
-    </div>
+    <img class="logo" src="http://podify.localhost:3000/packs/media/logo/navbar-9431a977.png">
   </div>
+
+  <aside class="sidebar" :class="{ active: active }">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" data-method="delete" href="/user/sign_out">
+          Sign out
+        </a>
+      </li>
+    </ul>
+  </aside>
+</div>
 
 </template>
 
 <script>
-  export default {
+export default {
+  data () {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.active = !this.active;
+    }
   }
+}
 </script>
