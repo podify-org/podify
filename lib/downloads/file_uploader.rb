982d8312bd3ac4178875e2f39d2814abdeb01de8
Max Hollmann <maxhollmann@gmail.com>
2020-05-19 21:34:11 +0200

    Nicer storage location

--- lib/downloads/file_uploader.rb
+++ lib/downloads/file_uploader.rb
@@ -4,5 +4,19 @@ module Downloads
                                host: Rails.application.routes.default_url_options[:host],
                                disposition: 'attachment'
     plugin :determine_mime_type, analyzer: :mimemagic
+
+    def generate_location(io, record: nil, **context)
+      [
+        'downloads',
+        sanitize_filename(record.author.presence || 'no_author'),
+        sanitize_filename(context[:metadata]['filename']),
+      ].join('/')
+    end
+
+    private
+
+    def sanitize_filename(name)
+      name.strip.gsub(/[^0-9A-Za-z.\-]+/, '_')
+    end
   end
 end
