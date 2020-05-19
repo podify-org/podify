3ab0143dddc192f2e44082198b8709e6b3b10d2b
Max Hollmann <maxhollmann@gmail.com>
2020-05-19 21:06:50 +0200

    Animated hamburger button

--- app/javascript/components/navbar.vue
+++ app/javascript/components/navbar.vue
@@ -2,20 +2,23 @@
 
 <div>
   <div class="navbar navbar-dark fixed-top bg-dark">
-    <button class="navbar-toggler"
+    <button class="navbar-toggler hamburger hamburger--arrow"
+            :class="{ 'is-active': active }"
             type="button"
-            aria-controls="navbar-menu"
+            aria-controls="sidebar"
             aria-expanded="false"
             aria-label="Toggle navigation"
             @click="toggleSidebar()"
             >
-      <span class="navbar-toggler-icon"></span>
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
     </button>
 
     <img class="logo" src="../../assets/images/logo/navbar.png">
   </div>
 
-  <aside class="sidebar" :class="{ active: active }">
+  <aside id="sidebar" class="sidebar" :class="{ active: active }">
     <ul class="top nav">
       <li class="nav-item">
         <a class="nav-link disabled">
