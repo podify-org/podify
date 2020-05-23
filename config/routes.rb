78979537bd578647ace0e955b6b127ee0217775f
Max Hollmann <maxhollmann@gmail.com>
2020-05-23 18:13:07 +0200

    Remove stuff that was replaced by GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,6 @@ Rails.application.routes.draw do
   root to: 'vue_apps#main'
   devise_for :user, class_name: 'DeviseUser'
 
-  resources :requests, only: %i[create]
-
-  resources :sources, only: %i[index show] do
-    resources :downloads, only: [] do
-      member do
-        get 'download', action: :serve, as: :serve
-      end
-    end
-  end
-
   get 'rss/:token', to: 'feeds#show', as: :feed, constraints: { format: :rss }
 
   mount Downloads::FileUploader.download_endpoint => '/download'
